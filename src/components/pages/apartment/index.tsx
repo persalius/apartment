d8799dc213c4846e8e4a0f08f618b15b63fdc4de
Shevchenko Sergey <persalius@gmail.com>
2025-10-14 21:37:04 +0300

    asd

--- src/components/pages/apartment/index.tsx
+++ src/components/pages/apartment/index.tsx
@@ -17,6 +17,7 @@ import { formatUAH } from "@/shared/utils/curency";
 import { Button } from "@/components/ui/button";
 import { PHONE_NUMBER } from "@/shared/constants/phone";
 import MapActions from "@/components/mapActions";
+import GoogleMap from "@/components/googleMap";
 
 interface Props {
   apartmentId: string;
@@ -94,16 +95,7 @@ const ApartmentPage = ({ apartmentId }: Props) => {
       )}
 
       <MapActions address={apartment.address} />
-
-      <iframe
-        className="mt-4 rounded-lg"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1026.9448714256275!2d34.328915261719175!3d48.654121968346296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sru!2sua!4v1760466380555!5m2!1sru!2sua"
-        width="100%"
-        height="300"
-        allowFullScreen={false}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <GoogleMap />
 
       <div className="mt-8">
         <p className="font-bold text-cyan-700 text-4xl">
