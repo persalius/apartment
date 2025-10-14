3b9b78ef82e31d7591334f9c7ff80028ae244619
Shevchenko Sergey <persalius@gmail.com>
2025-10-14 21:39:26 +0300

    asd

--- src/components/googleMap/index.tsx
+++ src/components/googleMap/index.tsx
@@ -3,7 +3,6 @@ const GoogleMap = () => {
     <iframe
       className="mt-4 rounded-lg w-full h-72"
       src="https://www.google.com/maps?q=48.65638160038468,34.33310460857509&z=15&output=embed"
-      loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
     ></iframe>
   );
